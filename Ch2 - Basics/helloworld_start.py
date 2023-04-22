0315a70d657c95e58d489d5c9393f174867c60bb
Yaroslav Odintsov <yaroslavod@gmail.com>
2023-04-22 16:38:08 -0400

    modified:   Ch2 - Basics/helloworld_start.py modified:   Ch2 - Basics/variables_start.py

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -4,3 +4,10 @@
 #
 
 
+def main():
+    print("hello world!")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -22,16 +22,37 @@ print(mytuple)
 print(mydict)
 
 # re-declaring a variable works
+myint = "abs"
+print (myint)
 
 # to access a member of a sequence type, use []
+# vibor 2 i 1 elementa v spiske
+print(mylist[2])
+print(mytuple[1])
 
 # use slices to get parts of a sequence
+# vibor s 1 po 5 elementa v spiske i s wagom 2
+print(mylist[1:5])
+print(mylist[1:5:2])
 
 # you can use slices to reverse a sequence
+# vivod spiska v obratnom poryadke s pomowu -1
+print(mylist[::-1])
 
 # dictionaries are accessed via keys
+print(mydict["one"])
 
 # ERROR: variables of different types cannot be combined
+print("string type " + str(123))
 
 # Global vs. local variables in functions
+def someFunction():
+    global mystr
+    mystr = "def"
+    print(mystr)
 
+someFunction()
+print(mystr)
+
+del mystr
+print(mystr)
